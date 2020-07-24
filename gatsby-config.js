5d13e6d244839090206280ec06f4341986ae5300
Giorgos kalmoukis <giorgoskalmoukis@gmail.com>
2020-07-24 20:48:56 +0300

    Update static sources

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,11 +33,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/`,
           },
           {
             name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            url: `https://www.instagram.com/`,
           },
         ],
       },
@@ -52,9 +52,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Geradinesmew - Another meow in the wall.`,
+        short_name: `geradinesmew-blog`,
+        description: `γατα ειμαι παρατα με`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
